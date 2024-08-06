9f23e83782f4e5b74c1c827d7aee38cf2253d26c
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-08-06 11:21:42 +0200

    "Refactor App.vue to use Header component and fetch products data from API; remove HelloWorld.vue component"

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,113 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <Header />
+    <div class="container">
+      <router-view :products="products" />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import { defineComponent, ref, onMounted } from "vue";
+import Header from "./components/Header.vue";
+
+/**
+ * The main application component.
+ *
+ * @component
+ */
+export default defineComponent({
+  /**
+   * The name of the component.
+   *
+   * @type {string}
+   */
+  name: "App",
+
+  /**
+   * The components used by this component.
+   *
+   * @type {object}
+   */
+  components: {
+    /**
+     * The header component.
+     *
+     * @type {Header}
+     */
+    Header,
+  },
+
+  /**
+   * The setup function for the component.
+   *
+   * @returns {object} The component's data and methods.
+   */
+  setup() {
+    /**
+     * The products data.
+     *
+     * @type {array}
+     */
+    const products = ref([]);
+
+    /**
+     * Fetches the products data from the API when the component is mounted.
+     *
+     * @async
+     */
+    onMounted(async () => {
+      try {
+        /**
+         * The response from the API.
+         *
+         * @type {Response}
+         */
+        const res = await fetch("https://fakestoreapi.com/products");
+
+        /**
+         * Sets the products data from the API response.
+         *
+         * @type {array}
+         */
+        products.value = await res.json();
+      } catch (error) {
+        /**
+         * Logs an error message if the fetch fails.
+         *
+         * @param {Error} error The error object.
+         */
+        console.error("Failed to fetch products:", error);
+      }
+    });
+
+    /**
+     * Returns the component's data and methods.
+     *
+     * @returns {object}
+     */
+    return {
+      /**
+       * The products data.
+       *
+       * @type {array}
+       */
+      products,
+    };
+  },
+});
+</script>
+
+<style>
+header {
+  background-color: #f8f8f8;
+  padding: 1em;
+  text-align: center;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 2em;
 }
 </style>
