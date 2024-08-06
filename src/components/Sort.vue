953d96ac39e93d03c92da4c380fc0d0c732ba65a
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-08-06 13:45:04 +0200

    "Added Sort.vue component with a select element for sorting options"

---
+++ src/components/Sort.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="sort">
+      <select @change="handleSortChange" v-model="sortOrder">
+        <option value="">Default</option>
+        <option value="low-to-high">Price: Low to High</option>
+        <option value="high-to-low">Price: High to Low</option>
+      </select>
+    </div>
+  </template>
+  
