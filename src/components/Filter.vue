9bdfbcfdb14e27de56b088fe3ee003ca07a4dc96
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-08-06 13:42:40 +0200

    "Added Filter.vue component with a select dropdown for categories"

---
+++ src/components/Filter.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="filter">
+      <select @change="handleCategoryChange" v-model="selectedCategory">
+        <option value="">All Categories</option>
+        <option v-for="category in categories" :key="category" :value="category">
+          {{ category }}
+        </option>
+      </select>
+    </div>
+  </template>
+  
