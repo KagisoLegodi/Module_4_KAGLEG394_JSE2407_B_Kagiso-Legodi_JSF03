0c3bdb0a7942c32ddba1bdc8026e8f206f68e233
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-08-06 13:46:50 +0200

    "Removed comment from router import statement"

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router"; // We will set up this file next
+import router from "./router";
 
 createApp(App).use(router).mount("#app");
