16c93dcdc62363fa2435a16fc047be4827af00e5
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-08-06 13:43:03 +0200

    "Added script section to Filter.vue component with props, emits, and setup function"

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -9,3 +9,31 @@
     </div>
   </template>
   
+  <script>
+import { ref, watch } from "vue";
+
+export default {
+  name: "Filter",
+  props: {
+    categories: Array,
+    initialCategory: String,
+  },
+  emits: ["update:category"],
+  setup(props, { emit }) {
+    const selectedCategory = ref(props.initialCategory || "");
+
+    const handleCategoryChange = () => {
+      emit("update:category", selectedCategory.value);
+    };
+
+    watch(selectedCategory, () => {
+      handleCategoryChange();
+    });
+
+    return {
+      selectedCategory,
+      handleCategoryChange,
+    };
+  },
+};
+</script>
